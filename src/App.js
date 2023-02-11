593d907878a650165e995c4744c0a05f12d447f6
Sergei Gordeev <gordeser@cvut.cz>
2023-02-11 16:55:00 +0100

    Add structure of the app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <h1>Pomodoro Timer</h1>
+
+        <div className="timer">
+            <span>00</span>
+            <span>:</span>
+            <span>00</span>
+        </div>
+
+        <div className="buttons">
+            <button>Start</button>
+            <button>Stop</button>
+            <button>Reset</button>
+        </div>
+
 
     </div>
   );
