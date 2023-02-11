e75ab7779d614944b69d059ab71d44454a774fe6
Sergei Gordeev <gordeser@cvut.cz>
2023-02-11 17:01:47 +0100

    Add initial state of time

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [timeLeft, setTimeLeft] = useState(25 * 60); // initial state = 25 minutes
+
+    const minutes = timeLeft/60
+    const seconds = timeLeft%60
+
   return (
     <div className="App">
         <h1>Pomodoro Timer</h1>
 
         <div className="timer">
-            <span>00</span>
+            <span>{minutes}</span>
             <span>:</span>
-            <span>00</span>
+            <span>{seconds}</span>
         </div>
 
         <div className="buttons">
