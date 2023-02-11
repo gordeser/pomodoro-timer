8d2ca6ee375d4cada1106e6d9a0825f613fd06d4
Sergei Gordeev <gordeser@cvut.cz>
2023-02-11 17:19:33 +0100

    Add start time function

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import {useState} from "react";
 
 function App() {
     const [title, setTitle] = useState('Let the countdown begin!')
-    const [timeLeft, setTimeLeft] = useState(25 * 60); // initial state = 25 minutes
+    const [timeLeft, setTimeLeft] = useState(10); // initial state = 25 minutes
 
     const minutes = Math.floor(timeLeft/60)
     const seconds = (timeLeft%60).toString().padStart(2, '0')
+
+    const startTimer = () => {
+        setInterval(() => {
+            setTimeLeft(timeLeft => {
+                if (timeLeft >= 1)
+                    return timeLeft-1;
+
+                return 0
+            })
+        }, 1000)
+    }
 
   return (
     <div className="App">
@@ -20,7 +31,7 @@ function App() {
         </div>
 
         <div className="buttons">
-            <button>Start</button>
+            <button onClick={startTimer}>Start</button>
             <button>Stop</button>
             <button>Reset</button>
         </div>
